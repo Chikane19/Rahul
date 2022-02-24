34344760ec272e2fb1bdf0bdb9138a90c9303fbe
Chikane19 <99965114+Chikane19@users.noreply.github.com>
2022-02-24 15:44:11 +0530

    Add files via upload

---
+++ Rahul-boot/Rahul-boot/src/main/java/com/nse/RahulBootApplication.java
@@ -0,0 +1,13 @@
+package com.nse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RahulBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RahulBootApplication.class, args);
+	}
+
+}
---
+++ Rahul-boot/Rahul-boot/src/main/java/com/nse/service/Service.java
@@ -0,0 +1,23 @@
+package com.nse.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.nse.entity.Portfolio;
+import com.nse.entity.Share;
+
+
+@org.springframework.stereotype.Service
+public interface Service {
+
+	public Portfolio createPortfolio(Portfolio p);
+
+	public List<Portfolio> showPortfolio();
+
+	public boolean buyShare(Integer shareId, Integer portfolioId);
+
+	public boolean sellShare(Integer shareId, Integer portfolioId);
+	public Share createShare(Share s);
+}
